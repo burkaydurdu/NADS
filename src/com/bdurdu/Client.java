77dca29da0af59895444100bec497ffee5da1a0f
burkay <burkaydurdu@outlook.com>
2018-05-16 15:42:18 +0300

    created server side client side

--- src/com/bdurdu/Client.java
+++ src/com/bdurdu/Client.java
@@ -1,4 +1,54 @@
 package com.bdurdu;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class Client {
+
+    private Image image;
+    private BufferedImage bufferedImage;
+    private Byte[] bytes;
+    private int port;
+
+    private final static String SERVER_NAME = "localhost";
+
+    public Client(int port) {
+        this.port = port;
+    }
+
+    private void onCreateConnection() {
+        try {
+
+            System.out.println("Connection : " + SERVER_NAME + " PORT : " +port);
+            Socket client = new Socket(SERVER_NAME, port);
+
+            System.out.println("Just connected to "
+                    + client.getRemoteSocketAddress());
+
+            DataInputStream in = new DataInputStream(client.getInputStream());
+            System.out.println(in.readUTF());
+            System.out.println(in.readUTF());
+
+            DataOutputStream out = new DataOutputStream(client.getOutputStream());
+
+            out.writeUTF("Hello from "
+                    + client.getLocalSocketAddress());
+            out.writeUTF("client: hello to server");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void getImage() {
+
+    }
+
+    private void sendImage() {
+
+    }
 }
--- src/com/bdurdu/Server.java
+++ src/com/bdurdu/Server.java
@@ -1,4 +1,64 @@
 package com.bdurdu;
 
-public class Server {
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
+public class Server extends Thread {
+
+    private ServerSocket serverSocket;
+
+    public Server(int port)
+    {
+        try {
+            serverSocket = new ServerSocket(port);
+            serverSocket.setSoTimeout(180000);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run()
+    {
+        while(true)
+        {
+            try
+            {
+                Socket server = serverSocket.accept();
+                DataInputStream din=new DataInputStream(server.getInputStream());
+                DataOutputStream dout=new DataOutputStream(server.getOutputStream());
+
+                dout.writeUTF("server: -i am greeting server");
+                dout.writeUTF("server:- hi! hello client");
+
+                System.out.println(din.readUTF());
+                System.out.println(din.readUTF());
+
+                BufferedImage img=ImageIO.read(ImageIO.createImageInputStream(server.getInputStream()));
+
+                System.out.println("Image received!!!!");
+                //lblimg.setIcon(img);
+            }
+            catch(SocketTimeoutException st)
+            {
+                System.out.println("Socket timed out!");
+                break;
+            }
+            catch(IOException e)
+            {
+                e.printStackTrace();
+                break;
+            }
+            catch(Exception ex)
+            {
+                System.out.println(ex);
+            }
+        }
+    }
 }
